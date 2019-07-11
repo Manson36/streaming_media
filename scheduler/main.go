1990dd2eb1ac58cf65af476c3f8eb5eac99a5628
Manson36 <469211209@qq.com>
2019-07-11 15:55:14 +0800

    [scheduler]finish

--- scheduler/main.go
+++ scheduler/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"github.com/julienschmidt/httprouter"
+	"github.com/streaming_media/scheduler/taskrunner"
+	"math"
+	"net/http"
+)
+
+func RegisterHandlers() *httprouter.Router {
+	router := httprouter.New()
+
+	router.GET("/video-delete-record/:vid-id", vidDelRecHandler)
+
+	return router
+}
+
 func main() {
-	
+	go taskrunner.Start()
+
+	r := RegisterHandlers()
+	http.ListenAndServe(":9001", r)//这里本身就是一个阻塞的模式，可以直接使用goroutine
 }
--- scheduler/taskrunner/trmain.go
+++ scheduler/taskrunner/trmain.go
@@ -1,1 +1,33 @@
 package taskrunner
+
+import "time"
+
+type Worker struct {
+	ticker *time.Ticker //初始化一个ticker，不断接收从系统发过来的时间，达到我们想要的时间间隔的时候触发任务
+	runner *Runner
+}
+
+func NewWorker(interval time.Duration, r *Runner) *Worker {
+	return &Worker{
+		ticker: time.NewTicker(interval * time.Second),
+		runner: r,
+	}
+}
+
+func (w *Worker) startWorker() {
+	for {
+		select {
+		case <-w.ticker.C:
+			go w.runner.StartAll()
+		}
+	}
+}
+
+func Start() {
+	//Start video file cleaning
+	r := NewRunner(3, true, VideoClearDispatcher, VideoClearExecutor)
+	w := NewWorker(3, r)
+	go w.startWorker()
+
+	//somethings else: 可以创建多个r，w， go w.startWorker 同时执行多个定时器
+}
